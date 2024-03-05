a5f92b2db8b677e257de0eb9f610dcf1a2dcc173
Moritz <moritz.stuewe@yahoo.de>
2024-03-05 17:40:54 +0100

    BilderGemacht, Bildschirmbewegung, neue Teller spawnen

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import random
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((1280, 720), pygame.RESIZABLE)
 clock = pygame.time.Clock()
 running = True
 dt = 0
@@ -20,16 +20,23 @@ class Wall(pygame.sprite.Sprite):
         self.image.fill("black")
         self.rect = self.image.get_rect(**kwargs)
 
+class Plate(pygame.sprite.Sprite):
+    def __init__(self, **kwargs) -> None:
+        super().__init__()
+        self.image = pygame.transform.scale2x(pygame.image.load("assets/plate.png").convert_alpha())
+        self.rect = self.image.get_rect(**kwargs)
+
+
+
 class Toast(pygame.sprite.Sprite):
     v = 6
     m = 1
-    def __init__(self, pos) -> None:
+    def __init__(self, pos, jam_or_peanut) -> None:
         super().__init__()
         self.position = pygame.Vector2(pos)
         self.velocity = pygame.Vector2()
 
-        self.image = pygame.Surface((100, 100))
-        
+        self.image = pygame.transform.scale_by(pygame.image.load("assets/jamToast.png" if jam_or_peanut else "assets/peanutToast.png").convert_alpha(), 2.5)
         self.rect = self.image.get_rect(center=self.position.xy)
 
         self.can_jump = True
@@ -81,25 +88,37 @@ class Toast(pygame.sprite.Sprite):
     def move(self, direction):
         self.position.x -= 300 * dt * direction
 
+class Camera:
+    def __init__(self, surface: pygame.Surface) -> None:
+        self.offset = pygame.Vector2()
+        self.surface = surface
+
+    def draw(self, group: pygame.sprite.Group):
+        for sprite in group.sprites():
+            pos = (pygame.Vector2(sprite.rect.topleft) + self.offset).xy
+            if isinstance(sprite, Plate) and pos.y - 250 > screen.get_height():
+                sprite.kill()
+                continue
+            self.surface.blit(sprite.image, pos)
 
 
 
-toastA = Toast((500, 500))
-toastB = Toast((500, 500))
+
+toastA = Toast((500, 500), True)
+toastB = Toast((500, 500), False)
 toastA.other_toast, toastB.other_toast = toastB, toastA
-toastA.image.fill("red")
-toastB.image.fill("green")
+
 toasts = pygame.sprite.Group(toastA, toastB)
 
 wall_group = pygame.sprite.Group()
 wall_group.add(Wall((screen.get_width(), 10), topleft=(0, screen.get_height() - 10)))
 
-for _ in range(10):
-    width = random.randint(20, 200)
-    wall_group.add(Wall((width, 40), bottomleft=(random.randint(10, screen.get_width()-10-width), random.randint(0, 600))))
-
-
 font = pygame.font.SysFont("arial", 18)
+cam = Camera(screen)
+
+for _ in range(10):
+    wall_group.add(Plate(bottomleft=(random.randint(10, screen.get_width()-10-64), random.randint(0, 600) - cam.offset.y)))
+
 
 while running:
     for event in pygame.event.get():
@@ -110,6 +129,10 @@ while running:
                 case pygame.K_SPACE | pygame.K_w:
                    if toastA.can_jump:
                     toastA.jump()
+                case pygame.K_q:
+                    toastA.position = toastB.position.xy + pygame.Vector2(0, -10)
+                case pygame.K_RETURN:
+                    toastB.position = toastA.position.xy + pygame.Vector2(0, -10)
                 case pygame.K_UP:
                     if toastB.can_jump:
                         toastB.jump()
@@ -130,13 +153,23 @@ while running:
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("blue")
-    wall_group.draw(screen)
-    toasts.draw(screen)
+    cam.draw(wall_group)
+    cam.draw(toasts)
     # text = font.render(f"{toast.velocity.y} {toast.rect.bottom} {screen.get_height()} {toast.position.distance_to(pygame.Vector2(pygame.mouse.get_pos()))}", False, "black")
     # screen.blit(text, (100, 100))
-    
+
+
+    while len(wall_group.sprites()) <= 10:
+        width = 64
+        new_wall = Plate(bottomleft=(random.randint(10, screen.get_width()-10-width), random.randint(-1000, 1000) - cam.offset.y))
+        if not pygame.sprite.spritecollide(new_wall, wall_group, (False, False)): 
+            wall_group.add(new_wall)
+
+
     # flip() the display to put your work on screen
     pygame.display.flip()
+
+    cam.offset.y = screen.get_height() -max(toastA.rect.bottom, toastB.rect.bottom) - 100
 
     # limits FPS to 60
     # dt is delta time in seconds since last frame, used for framerate-
